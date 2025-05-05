a7736acee37590f911a624f4e23c1bf18b7f0457
Emmett Infante <eminfante15@gmail.com>
2025-05-04 20:08:36 -0400

    Update plaid.py

--- plaid.py
+++ plaid.py
@@ -2,15 +2,9 @@ import plaid_transactions
 import plaid_assets
 from dotenv import load_dotenv
 import pandas as pd
-import os
 
-# Load .env file
-load_dotenv()
-
-# Get your token
-token = os.getenv('lunchmoney_token')
+plaid_transactions.transactions()
 
 
-plaid_transactions.transactions(token)
 
 # print(plaid_assets.assets(token))
